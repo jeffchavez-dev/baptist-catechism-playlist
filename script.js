03bbb7f710cbe537013a17346c61bd6c3a1e7210
Jeff Chavez <jeff.chavez0828@gmail.com>
2023-02-13 21:12:19 +0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+let audio = document.getElementById("audio"); // Take the audio element
+let time = document.querySelector(".time"); // Take the audio track
+let btnPlay = document.querySelector(".play"); // Take the play button
+let btnPause = document.querySelector(".pause"); // Take the pause button
+let btnPrev = document.querySelector(". prev"); // Take the switch button of the previous track
+let btnNext = document.querySelector(".next"); // Take the button to switch the next track
